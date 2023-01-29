e096160af79810bc11e1fbbfccb41436246b564f
Nick <gri.nik.v@yandex.ru>
2023-01-29 23:26:15 +0300

    Add ex 8-11 second lesson

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1 ,2 ,3 ,92 ,51 ,62 ,75 ,83 ,92};
+
+int n = array.Length;
+int find =92;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine (index);
+        break;
+    }
+    index ++;
+
+}
